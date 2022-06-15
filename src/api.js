d7ee820027e0767ef58658ea5e5a47609992f81a
Baidi Wang <baidiwang30@gmail.com>
2022-06-15 18:02:00 -0400

    done

--- src/api.js
+++ src/api.js
@@ -1,56 +1,8 @@
-// const axios = require('axios');
+const axios = require('axios');
 // // check the second part beginning video to why we not use method-override and destroy
 
 
 // //why don't we need await here? 
-// const fetchUsers = () => {
-//     return axios.get('/api/users');
-// }
-
-// const fethchRestaurants = () => {
-//     return axios.get('/api/restaurants');
-// }
-
-// const fetchReservations = (id) => {
-//     return axios.get(`/api/users/${id}/reservations`);
-// }
-
-
-// const deleteReservations = (id) => {
-//     return axios.delete(`/api/reservations/${id}`);
-// }
-
-
-// //check the video for this part
-// const createReservations = async({ userId, restaurantId }) => {
-//     const response = await axios.post(`/api/users/${userId}/reservations`, {
-//         restaurantId
-//     });
-//     return response.data;
-// };
-
-
-// const createUser = async(user) => {
-//     const response = await axios.post('/api/users', user);
-//     return response.data;
-// };
-
-
-// module.express = {
-//     fetchUsers,
-//     fethchRestaurants,
-//     fetchReservations,
-//     deleteReservations,
-//     createReservations,
-//     createUser
-// }
-
-
-
-
-
-const axios = require('axios');
-
 const fetchUsers = ()=> {
   return axios.get('/api/users');
 }
@@ -67,6 +19,7 @@ const deleteReservation = (id)=> {
   return axios.delete(`/api/reservations/${id}`);
 }
 
+// //check the video for this part
 const createReservation = async({ userId, restaurantId })=> {
   const response = await axios.post(`/api/users/${userId}/reservations`, {
     restaurantId
